2ffad358403090171ff7d79f096d6e97e2afbcd6
RonVol <rvolnets@gmail.com>
2024-03-15 20:14:09 +0200

    simulate user input funcs

--- app/Controller.py
+++ app/Controller.py
@@ -1,30 +1,59 @@
-from .UI.main_ui import UI
-from .Core.main_core import Main_Core
-import xgboost as xgb
+from app.Core.main_core import Main_Core
+from app.UI.main_ui import Main_UI
+import app.config as config
+from app.data_loader import DataLoader
 
 class Controller:
-    def __init__(self, core : Main_Core, ui : UI):
+    def __init__(self, core : Main_Core, ui : Main_UI):
         self.core = core
         self.ui = ui
 
     def run_ui(self):
-        self.ui.run_ui()
+        #self.ui.run_ui()
 
-    def get_user_input(self):
-        self.inp = self.ui.get_simulated_user_input()
-        if not self.load_model:
-            # send to ui that failed to load model
-            print("...")
+        #for testing without ui
+        self.validate_user_input()
+        self.start_main_pipeline()
 
-    def load_model(self):
-        model_fpath = self.inp['model_path']
-        # if .npy,if .mode..if pickle...
-        try:
-            model = xgb.Booster()
-            self.model = model.load_model(model_fpath)  
-            print(f"Model loaded from {model_fpath}")
-            return True
-        except:
-            print(f"Failed to load model from {model_fpath}")
-        return False
+    def validate_user_input(self):
+        inp = self.ui.get_simulated_user_input()# for now simulated, later change it.
+        #if config.validate_user_input(inp): should perform validation to the schema
+        self.user_input = inp
+    
+    def start_main_pipeline(self):
+        dataloader = self.create_dataloader()
+        parameters = self.get_user_input_parameters()
+        attacks = self.get_user_chosen_attacks()
+        defenses = self.get_user_chosen_defenses()
+        self.core.run_pipeline(dataloader,parameters,attacks,defenses)
+
+    def get_user_chosen_attacks(self):
+        chosen_attacks = self.user_input['chosen_attacks']
+        return chosen_attacks
+    
+    def get_user_chosen_defenses(self):
+        chosen_defenses = self.user_input['chosen_defenses']
+        return chosen_defenses
+
+    def get_user_input_parameters(self):
+        nb_classes = self.user_input['nb_classes']
+        nb_features = self.user_input['nb_features']   
+        return {'nb_classes':nb_classes, 'nb_features':nb_features}
+
+    def create_dataloader(self):
+        dataloader = DataLoader()
+        model_fpath = self.user_input['model_path']
+        x_test_fpath = self.user_input['x_test_path']
+        y_test_fpath = self.user_input['y_test_path']
+        model_library = self.user_input['model_library']
+
+        if not dataloader.load_model(model_library, model_fpath):
+            # pass to ui that failed to load model
+            pass
+
+        if not dataloader.load_test(x_test_fpath,y_test_fpath):
+            # pass to ui that failed to load test
+            pass
+
+        return dataloader
 
--- app/UI/main_ui.py
+++ app/UI/main_ui.py
@@ -1,10 +1,10 @@
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
 
-from ..config import supported_libraries, supported_attacks
+from app.config import supported_libraries, supported_attacks, supported_defenses
 from utils.mnist_xgboost import get_model_path,get_x_test_path,get_y_test_path # for simulated input
 
-class UI():
+class Main_UI():
 
     def __init__(self):
         print("UI init")
@@ -20,14 +20,20 @@ class UI():
         dpg.start_dearpygui()
         dpg.destroy_context()
 
-    def get_simulated_user_input():
+    def get_simulated_user_input(self):
         chosen_model_file_path = get_model_path()
         chosen_model_library = supported_libraries['XGBoost']
         chosen_attacks = [supported_attacks['FGSM']] # list of chosen attacks
+        chosen_defenses = []
         chosen_x_test = get_x_test_path()
         chosen_y_test = get_y_test_path()
+        nb_classes = 10
+        nb_features = 28*28
         return {"model_path":chosen_model_file_path,
-                "model_library": chosen_model_library,
+                "model_library":chosen_model_library,
                 "chosen_attacks":chosen_attacks,
-                "x_test":chosen_x_test,
-                "y_test":chosen_y_test}
+                "chosen_defenses":chosen_defenses,
+                "x_test_path":chosen_x_test,
+                "y_test_path":chosen_y_test,
+                "nb_classes":nb_classes,
+                "nb_features":nb_features}
