7808a78a19ea54b528540bb015d32011cd41fa03
RonVol <rvolnets@gmail.com>
2024-06-27 07:21:05 +0300

    yoad's update - browse/back buttons

--- app/UI/main_ui.py
+++ app/UI/main_ui.py
@@ -8,10 +8,46 @@ class Main_UI:
         self.def_x_path = "models/iris_xgboost_x_test.npy"
         self.def_y_path = "models/iris_xgboost_y_test.npy"
         self.setup_ui()
+ 
 
     def set_controller(self, controller):
         self.controller = controller
 
+    def load_callback(self, sender, app_data, user_data):
+        self.model_dialog(user_data)
+
+    def load_callback_xy(self, sender, app_data, user_data):
+        self.xy_dialog(user_data)
+       
+    
+    def file_selected_callback(self, sender, app_data, user_data):
+        selected_file = app_data["file_path_name"]
+        target_input_id = user_data
+        print(user_data)
+        dpg.set_value(target_input_id, selected_file)
+        if dpg.does_item_exist("model_dialog_id"):
+            dpg.delete_item("model_dialog_id")
+        if dpg.does_item_exist("xy_dialog_id"):
+            dpg.delete_item("xy_dialog_id")
+        dpg.show_item("Main_Window")
+
+    def model_dialog(self, data):
+        if not dpg.does_item_exist("model_dialog_id"):
+            with dpg.file_dialog(directory_selector=False, show=False, callback=self.file_selected_callback, cancel_callback=self.cancel_callback, user_data=data,  id="model_dialog_id", height=600, width=600):
+                dpg.add_file_extension(".model", color=(150, 255, 150, 255))
+        dpg.show_item("model_dialog_id")
+        dpg.hide_item("Main_Window")
+
+    def xy_dialog(self, data):
+        if not dpg.does_item_exist("xy_dialog_id"):
+            with dpg.file_dialog(directory_selector=False, show=False, callback=self.file_selected_callback, cancel_callback=self.cancel_callback, user_data=data, id="xy_dialog_id", height=600, width=600):
+                dpg.add_file_extension(".npy", color=(150, 255, 150, 255))
+        dpg.show_item("xy_dialog_id")
+        dpg.hide_item("Main_Window")
+    
+    def cancel_callback(self, sender, app_data, user_data):
+        dpg.show_item("Main_Window")
+    
     def setup_ui(self):
         dpg.create_context()
         with dpg.window(label="Main Window", tag="Main_Window", width=1280, height=720, no_move=True):
@@ -19,7 +55,8 @@ class Main_UI:
                     dpg.add_text("Enter File Paths")
                 with dpg.group(horizontal=True):  # Label on the left
                     dpg.add_text("Model File Path: ")
-                    self.model_path_id = dpg.add_input_text(default_value=self.def_model_path, width=-1)
+                    self.model_path_id = dpg.add_input_text(default_value=self.def_model_path)
+                    dpg.add_button(label="browse", callback=self.load_callback, user_data=self.model_path_id)
                             # Creating the dropdown for library selection
                 library_names = [lib["name"] for lib in supported_libraries.values()]
                 with dpg.group(horizontal=True):
@@ -28,44 +65,66 @@ class Main_UI:
 
                 with dpg.group(horizontal=True):  # Label on the left
                     dpg.add_text("X File Path: ")
-                    self.x_path_id = dpg.add_input_text(default_value=self.def_x_path, width=-1)
+                    self.x_path_id = dpg.add_input_text(default_value=self.def_x_path)
+                    dpg.add_button(label="browse", callback=self.load_callback_xy, user_data=self.x_path_id)
                 with dpg.group(horizontal=True):  # Label on the left
                     dpg.add_text("Y File Path: ")
-                    self.y_path_id = dpg.add_input_text(default_value=self.def_y_path, width=-1)
-                
+                    self.y_path_id = dpg.add_input_text(default_value=self.def_y_path)
+                    dpg.add_button(label="browse", callback=self.load_callback_xy, user_data=self.y_path_id)
                 dpg.add_button(label="Load", callback=self.load_files)
+    
+    def create_popup(self, text, window):
+        dpg.hide_item(window)
+        with dpg.window( tag="pop_up",show=True, width=400, height=200):
+            with dpg.group(horizontal=False):
+                dpg.add_text(text, tag="popup_text")
+                dpg.add_button(label="OK", width=75,tag="popup_button", callback=lambda: self.popup_button(window))
+            
+        
+        
+                
+                
 
     def setup_attack_defense_window(self):
-        with dpg.window(label="Select Attacks and Defenses",tag="Select Attacks and Defenses", width=1280, height=720, no_move=True):
-            with dpg.group(horizontal=False):
-                dpg.add_text("Select Attacks")
-                for attack in supported_attacks:
-                    dpg.add_checkbox(label=attack, tag=f"attack_{attack}")
+        if not dpg.does_item_exist("Select Attacks and Defenses"):
+            with dpg.window(label="Select Attacks and Defenses",tag="Select Attacks and Defenses", width=1280, height=720, no_move=True):
+                with dpg.group(horizontal=False):
+                    dpg.add_text("Select Attacks")
+                    for attack in supported_attacks:
+                        dpg.add_checkbox(label=attack, tag=f"attack_{attack}")
 
-                dpg.add_button(label="Select All Attacks", callback=lambda: self.select_all(True, supported_attacks, prefix="attack_"))
-                dpg.add_button(label="Select None Attacks", callback=lambda: self.select_all(False, supported_attacks, prefix="attack_"))
+                    dpg.add_button(label="Select All Attacks", callback=lambda: self.select_all(True, supported_attacks, prefix="attack_"))
+                    dpg.add_button(label="Select None Attacks", callback=lambda: self.select_all(False, supported_attacks, prefix="attack_"))
+
+                    dpg.add_separator()
+
+                    dpg.add_text("Select Defenses")
+                    for defense in supported_defenses:
+                        dpg.add_checkbox(label=defense, tag=f"defense_{defense}")
+
+                    dpg.add_button(label="Select All Defenses", callback=lambda: self.select_all(True, supported_defenses, prefix="defense_"))
+                    dpg.add_button(label="Select None Defenses", callback=lambda: self.select_all(False, supported_defenses, prefix="defense_"))
 
                 dpg.add_separator()
+                with dpg.group(horizontal=False):
+                    dpg.add_text("Parameter Configuration Options")
+                    with dpg.group():
+                        # Using a radio button for exclusive selection
+                        self.param_config_option = dpg.add_radio_button(["Run on Default Parameters", 
+                                                                        "Optimize Attack and Defense Parameters", 
+                                                                        "Configure Manually Attack and Defense Parameters"], 
+                                                                        horizontal=False)
+                    dpg.add_button(label="Proceed",tag="proceed_with_selection", callback=self.on_proceed_with_selection, user_data="Select Attacks and Defenses")
+                    dpg.add_button(label="back", callback=self.back_to_loading)
+        else:
+            dpg.show_item("Select Attacks and Defenses")
+        
 
-                dpg.add_text("Select Defenses")
-                for defense in supported_defenses:
-                    dpg.add_checkbox(label=defense, tag=f"defense_{defense}")
 
-                dpg.add_button(label="Select All Defenses", callback=lambda: self.select_all(True, supported_defenses, prefix="defense_"))
-                dpg.add_button(label="Select None Defenses", callback=lambda: self.select_all(False, supported_defenses, prefix="defense_"))
-
-            dpg.add_separator()
-            with dpg.group(horizontal=False):
-                dpg.add_text("Parameter Configuration Options")
-                with dpg.group():
-                    # Using a radio button for exclusive selection
-                    self.param_config_option = dpg.add_radio_button(["Run on Default Parameters", 
-                                                                    "Optimize Attack and Defense Parameters", 
-                                                                    "Configure Manually Attack and Defense Parameters"], 
-                                                                    horizontal=False)
-                dpg.add_button(label="Proceed", callback=self.on_proceed_with_selection)
 
     def setup_parameter_configuration_window(self, selected_attacks, selected_defenses):
+        if dpg.does_item_exist("Configure Parameters"):
+            dpg.delete_item("Configure Parameters")
         with dpg.window(label="Configure Parameters",tag="Configure Parameters", width=1280, height=720, no_move=True):
             # Configure Attack Parameters
             with dpg.group(horizontal=False):
@@ -82,6 +141,7 @@ class Main_UI:
                                         dpg.add_checkbox(default_value=value, tag=f"{attack_config['name']}_{param}")
                                 else:
                                     dpg.add_input_text(default_value=str(value), width=200, tag=f"{attack_config['name']}_{param}")
+                                dpg.add_separator()
 
             dpg.add_separator()
 
@@ -103,27 +163,32 @@ class Main_UI:
 
             dpg.add_separator()
             dpg.add_button(label="Begin Run", callback=self.on_proceed_with_selection_manual_config)
+            dpg.add_button(label="back",callback=self.back_to_selection)
+        
 
-
-    def on_proceed_with_selection(self):
+    def popup_button(self, window_tag):
+        dpg.show_item(window_tag)
+        dpg.delete_item("pop_up")
+    def on_proceed_with_selection(self, sender, app_data, user_data):
         selected_option = dpg.get_value(self.param_config_option)
-        if(selected_option == ""):# if nothing is selected by clicking string is empty instead of default run
-            selected_option = "Run on Default Parameters"
-
         self.selected_attacks = [supported_attacks[attack] for attack in supported_attacks if dpg.get_value(f"attack_{attack}")]
         self.selected_defenses = [supported_defenses[defense] for defense in supported_defenses if dpg.get_value(f"defense_{defense}")]
+        if len(self.selected_attacks)==0 and len(self.selected_defenses)==0:
+            print(user_data)
+            self.create_popup("Please select at least 1 attack or defence to apply",user_data)
 
-        if selected_option == "Configure Manually Attack and Defense Parameters":
-            dpg.hide_item("Select Attacks and Defenses")  # Assuming this is the tag for the attack/defense selection window
-            self.setup_parameter_configuration_window(self.selected_attacks, self.selected_defenses)
         else:
-            # Pass the selections along with the chosen option (1 for default, 2 for optimize) back to the controller
-            if self.controller:
-                dpg.hide_item("Select Attacks and Defenses")
-                self.show_progress_window()
-                self.controller.handle_configuration(self.selected_attacks, self.selected_defenses, 1 if selected_option == "Run on Default Parameters" else 2)
+            if selected_option == "Configure Manually Attack and Defense Parameters":
+                dpg.hide_item("Select Attacks and Defenses")  # Assuming this is the tag for the attack/defense selection window
+                self.setup_parameter_configuration_window(self.selected_attacks, self.selected_defenses)
             else:
-                print("Controller not set!")
+                # Pass the selections along with the chosen option (1 for default, 2 for optimize) back to the controller
+                if self.controller:
+                    dpg.hide_item("Select Attacks and Defenses")
+                    self.show_progress_window()
+                    self.controller.handle_configuration(self.selected_attacks, self.selected_defenses, 1 if selected_option == "Run on Default Parameters" else 2)
+                else:
+                    print("Controller not set!")
 
     def on_proceed_with_selection_manual_config(self):
         # Gather updated configurations for attacks
@@ -170,14 +235,19 @@ class Main_UI:
             with dpg.group(horizontal=True, width=980/2):
                 dpg.add_spacer()  # Auto-adjusts to center the following widget
                 dpg.add_text("Starting pipeline...", tag="progress_text")
+                dpg.add_button(label="ANOTHER RUN",tag="reset", callback=self.reset_button, height=30, width= 60, show=False)
                 dpg.add_spacer()  # Ensures the text widget is centered
+                
             
             dpg.add_spacer(height=10)  # Adjust height for better vertical alignment
+            
 
             
-    def update_progress(self, message):
+    def update_progress(self, message, done = False):
         if dpg.does_item_exist("progress_text"):
             dpg.set_value("progress_text",dpg.get_value("progress_text")+'\n'+ message)
+            if done:
+                dpg.show_item("reset")
         else:
             print("Progress window is not open or progress text does not exist.")
 
@@ -186,6 +256,9 @@ class Main_UI:
         for item in items:
             dpg.set_value(f"{prefix}{item}", select)
     
+    def back_to_selection(self):
+        dpg.hide_item("Configure Parameters")
+        dpg.show_item("Select Attacks and Defenses")
     def load_files(self):
         model_path = dpg.get_value(self.model_path_id)
         x_path = dpg.get_value(self.x_path_id)
@@ -196,11 +269,28 @@ class Main_UI:
             success = self.controller.handle_load(model_path, x_path, y_path, selected_library)
             if success:
                 dpg.hide_item("Main_Window")  # Assuming "Main Window" is the tag of the main window
-                self.setup_attack_defense_window()
+                if not dpg.does_item_exist("Select Attacks and Defenses"):
+                    self.setup_attack_defense_window()
+                else:
+                    dpg.show_item("Select Attacks and Defenses")
             else:
-                print("Load unsuccessful.")
+                self.create_popup("Loading unsuccessful, please choose appropriate files","Main_Window")
         else:
             print("Controller not set!")
+    
+    def back_to_loading(self):
+        if self.controller:
+                dpg.hide_item("Select Attacks and Defenses")  # Assuming "Main Window" is the tag of the main window
+                dpg.show_item("Main_Window")
+        else:
+            print("Controller not set!")
+    
+    def reset_button(self):
+        dpg.delete_item("progress_window")
+        dpg.delete_item("Main_Window")
+        dpg.delete_item("Select Attacks and Defenses")
+        dpg.delete_item("Configure Parameters")
+        self.setup_ui()
     
     def run(self):
         dpg.create_viewport(title='Custom Title', width=1280, height=720)
